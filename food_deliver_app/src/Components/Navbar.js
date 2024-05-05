ff50410c249b8db3395ac47a8ae79def4fb556e0
Kalpani99 <kalpanidiwyanjali99@gmail.com>
2024-05-05 14:39:34 +0530

    update

--- food_deliver_app/src/Components/Navbar.js
+++ food_deliver_app/src/Components/Navbar.js
@@ -3,13 +3,8 @@ import Logo from '../Assets/Logo.svg'
 import {BsCart2} from 'react-icons/bs'
 import {HiOutlineBars, HiOutlineBars3} from 'react-icons/hi2'
 
-<<<<<<< Updated upstream
-import Box from '@mui/icons-material'
 
-import Box from '@mui/icons-material/Box'
-=======
 import Box from '@mui/material/Box'
->>>>>>> Stashed changes
 
 import Drawer from "@mui/material/Drawer"
 import List from "@mui/material/List"
