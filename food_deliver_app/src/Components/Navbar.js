ad9c0689bc930746c4718d23659d2202e1e03a94
Kalpani99 <kalpanidiwyanjali99@gmail.com>
2024-05-05 13:10:43 +0530

    updated

---
+++ food_deliver_app/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../Assets/Logo.svg'
+import {BsCart2} from 'react-icons/bs'
+import {HiOutlineBars} from 'react-icons/hi2'
+import {Box,Drawer,ListItem,ListItemButton,ListItemIcon,ListItemText} from '@mui/icons-material'
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
